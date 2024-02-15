251631c99f0aa4eca80f61f0b792ca8b02f43970
mohamedaliazouzi <61392012+mohamedaliazouzi@users.noreply.github.com>
2024-02-15 11:24:04 +0100

    commit

--- src/main/java/tn/esprit/spring/kaddem/KaddemApplication.java
+++ src/main/java/tn/esprit/spring/kaddem/KaddemApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class KaddemApplication {
-
+/* med*/
     public static void main(String[] args) {
         SpringApplication.run(KaddemApplication.class, args);
     }
